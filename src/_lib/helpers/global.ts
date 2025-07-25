7ed01d044a7cb30f8d5c8311d8602a71d871eef2
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2025-07-25 05:06:57 +0000

    Create Basic MCP Server

---
+++ src/_lib/helpers/global.ts
@@ -0,0 +1,53 @@
+interface FetchBooksParams {
+    query: string | null;
+    maxNumber: number;
+}
+
+async function fetchBooks({ query, maxNumber }: FetchBooksParams) {
+    const page = 1
+    const limit = maxNumber ?? 10
+    const inc = 'kind,id,etag,volumeInfo'
+    const queryValue = query ?? ''
+
+    if (maxNumber > 10)
+        console.warn(`Number of books cann't be more than 10 to avoid context length issue.`)
+
+    const baseUrl = 'https://api.freeapi.app/api/v1/public/books';
+    const queryParams = new URLSearchParams({
+        page: page.toString(),
+        limit: limit.toString(),
+        inc: inc,
+        query: queryValue
+    });
+    try {
+        const response = await fetch(`${baseUrl}?${queryParams}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.warn(data.data.data)
+        return {
+            content: [
+                {
+                    type: "text" as const,
+                    text: JSON.stringify(data.data.data),
+
+                },
+            ],
+        };
+    } catch (error) {
+        console.error('Error fetching books:', error);
+        return {
+            content: [
+                {
+                    type: "text" as const,
+                    text: `❌ Error fetching books ${error as string}`,
+                },
+            ],
+            isError: true,
+        };
+    }
+}
+
+export { fetchBooks };
+
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import { fetchBooks } from "./_lib/helpers/global";
+
+const server = new McpServer({
+  name: "book-details",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+server.tool('getAllBookInfo', "Fetch all the books information", {
+  query: z.string().describe("category of the books"),
+  maxNumber: z.number().describe("Maximum no of books")
+}, fetchBooks)
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.warn("Book MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
