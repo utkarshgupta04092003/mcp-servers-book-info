415544e70e7e66ada5096059da16e8ce1ed640d0
Utkarsh Gupta <63789702+utkarshgupta04092003@users.noreply.github.com>
2025-07-28 20:11:40 +0000

    Add Task Management Tools for DB Operations

--- src/_lib/data-fetchers/tasks.ts
+++ src/_lib/data-fetchers/tasks.ts
@@ -2,14 +2,49 @@ import { prisma } from '../helpers/db'
 
 interface TaskInput {
     title: string
-    description: string | null
+    description?: string
     status: boolean
     date: string
 }
 
-export async function getAllTasks({ completed }: { completed: boolean | null }) {
+type UpdatTaskParams = {
+    taskId: number
+    title?: string
+    description?: string
+    status?: boolean
+    date?: string
+}
+
+export async function getAllTasks({
+    dueDate,
+    completed,
+}: {
+    dueDate?: string
+    completed?: boolean | undefined
+}) {
     const taskList = await prisma.todo.findMany({
-        where: completed ? { completed: completed } : {},
+        where: {
+            AND: [
+                completed ? { completed: completed } : {},
+                dueDate
+                    ? {
+                          date: {
+                              gte: new Date(dueDate),
+                              lt: new Date(
+                                  new Date(dueDate).setDate(new Date(dueDate).getDate() + 1)
+                              ),
+                          },
+                      }
+                    : {},
+            ],
+        },
+        select: {
+            taskId: true,
+            title: true,
+            description: true,
+            completed: true,
+            date: true,
+        },
     })
     console.warn(`[INFO] Fetched all the task list of status: ${completed}: ${taskList}`)
     return {
@@ -18,14 +53,23 @@ export async function getAllTasks({ completed }: { completed: boolean | null }) 
 }
 
 export async function createNewTask({ title, description, status, date }: TaskInput) {
+    const taskId = await getNextId()
     const createdTask = await prisma.todo.create({
         data: {
+            taskId,
             title: title,
             description: description ?? '',
             completed: status,
             date: new Date(date),
             createdAt: new Date(),
             updatedAt: new Date(),
+        },
+        select: {
+            taskId: true,
+            title: true,
+            description: true,
+            completed: true,
+            date: true,
         },
     })
     console.warn(`[INFO] Created new task: ${createdTask}`)
@@ -37,4 +81,68 @@ export async function createNewTask({ title, description, status, date }: TaskIn
             },
         ],
     }
+}
+
+export async function updateTaskbyId({
+    taskId,
+    title,
+    description,
+    status,
+    date,
+}: UpdatTaskParams) {
+    const dataToUpdate: any = {}
+    if (title) dataToUpdate.title = title
+    if (description) dataToUpdate.description = description
+    if (status) dataToUpdate.completed = status
+    if (date) dataToUpdate.date = new Date(date)
+
+    if (Object.keys(dataToUpdate).length > 0) {
+        const updated = await prisma.todo.update({
+            where: {
+                taskId: taskId,
+            },
+            data: dataToUpdate,
+        })
+        return {
+            content: [{ type: 'text' as const, text: JSON.stringify(updated) }],
+        }
+    } else {
+        return {
+            content: [
+                {
+                    type: 'text' as const,
+                    text: `No data found to be updated`,
+                },
+            ],
+        }
+    }
+}
+
+export async function deleteTaskById({ taskId }: { taskId: number }) {
+    if (!taskId)
+        return {
+            content: [{ type: 'text' as const, text: 'Task Id is required' }],
+            isError: true,
+        }
+    const deleted = await prisma.todo.delete({
+        where: {
+            taskId,
+        },
+    })
+    return {
+        content: [{ type: 'text' as const, text: JSON.stringify(deleted) }],
+    }
+}
+
+async function getNextId() {
+    const latestTask = await prisma.todo.findMany({
+        select: {
+            taskId: true,
+        },
+        orderBy: {
+            taskId: 'desc',
+        },
+        take: 1,
+    })
+    return (latestTask?.[0]?.taskId ?? 0) + 1
 }
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,35 +1,78 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
-import { createNewTask, getAllTasks } from './_lib/data-fetchers/tasks'
 import z from 'zod'
+import {
+    createNewTask,
+    deleteTaskById,
+    getAllTasks,
+    updateTaskbyId,
+} from './_lib/data-fetchers/tasks'
 
 const server = new McpServer({
     name: 'taskManagement',
     version: '1.0.1',
 })
 
-server.tool(
+server.registerTool(
     'getAllTasks',
-    'Fetch/ Get list of all tasks',
     {
-        completed: z
-            .boolean()
-            .nullable()
-            .describe('Fetch the list of completed task or not completed task'),
+        description: `Today is ${new Date().toDateString()}. extract the tasks based on status pending or completed and based on date also. always add taskId in the final response`,
+        inputSchema: {
+            dueDate: z
+                .string()
+                .optional()
+                .describe(
+                    `The date mentioned for each tasks that is due date in YYYY-MM-DD format`
+                ),
+            completed: z
+                .boolean()
+                .optional()
+                .describe(
+                    `Completed should be true if user is asking for completed tasks else false `
+                ),
+        },
     },
     getAllTasks
 )
 
-server.tool(
+server.registerTool(
     'createNewTask',
-    'Create or add new Task to the list',
     {
-        title: z.string().describe('Task title to be saved'),
-        description: z.string().nullable().describe('Task description'),
-        status: z.boolean().default(false).describe('Status of the task e.g completed or not'),
-        date: z.string().datetime().describe('Due date in ISO 8601 format'),
+        description: `Create or add new task to the list based on the provided title, description (optional), status (optional) and dueDate in ISO String format`,
+        inputSchema: {
+            title: z.string().describe('Task title to be saved'),
+            description: z.string().optional().describe('Task description'),
+            status: z.boolean().default(false).describe('Status of the task e.g completed or not'),
+            date: z.string().describe('Due date in UTC format format'),
+        },
     },
     createNewTask
+)
+
+server.registerTool(
+    'updateTaskById',
+    {
+        description: `Update an existing task by its ID. You can update title, description, status, or due date`,
+        inputSchema: {
+            taskId: z.number().describe('ID of the task to update'),
+            title: z.string().optional().describe('New task title'),
+            description: z.string().optional().describe('New task description'),
+            status: z.boolean().optional().describe('New status of the task'),
+            date: z.string().datetime().optional().describe('New due date in UTC format'),
+        },
+    },
+    updateTaskbyId
+)
+
+server.registerTool(
+    'deleteTaskById',
+    {
+        description: `Delete any task by its taskId`,
+        inputSchema: {
+            taskId: z.number().describe('Task id for deleting from DB'),
+        },
+    },
+    deleteTaskById
 )
 
 async function main() {
